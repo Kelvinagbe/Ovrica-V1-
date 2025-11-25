57fcea4a5646f51887a1450d743a46a653246696
Kelvin Agbe <139677635+Kelvinagbe@users.noreply.github.com>
2025-11-26 00:43:22 +0100

    Create botinfo.js

---
+++ src/db/botinfo.js
@@ -0,0 +1,11 @@
+{
+  "botName": "OVRICA-V1",
+  "version": "1.0.0",
+  "owner": "KELVIN AGBE",
+  "ownerNumber": "2348109860102",
+  "channelLink": "https://whatsapp.com/channel/0029VajVvKSEquiIuUH1Rd2q",
+  "newsletterJid": "120363418958316196@newsletter",
+  "newsletterName": "🎭 Kelvin Tech",
+  "channelUrl": "https://whatsapp.com/channel/0029VbBODJPIiRonb0FL8q10",
+  "thumbnailUrl": "https://files.catbox.moe/gzrvvk.jpg"
+}
