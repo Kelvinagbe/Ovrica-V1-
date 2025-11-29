493b3efd4c9e478355ec62d05462ee5cb94a735a
Kelvin Agbe <139677635+Kelvinagbe@users.noreply.github.com>
2025-11-29 23:24:16 +0100

    Update menu.js

--- src/cmd/menu.js
+++ src/cmd/menu.js
@@ -1,6 +1,6 @@
 // commands/menu.js - Main menu with fancy reply using local image
 
-const { templates } = require('../templates');
+const { templates } = require('../../templates');
 const fs = require('fs');
 
 function getBotInfo(config) {
