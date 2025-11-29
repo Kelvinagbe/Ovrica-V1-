86594893ce745729ead10d9eff15aea61358cda3
Kelvin Agbe <139677635+Kelvinagbe@users.noreply.github.com>
2025-11-29 15:47:45 +0100

    Create energy.js

---
+++ commands/energy.js
@@ -0,0 +1,21 @@
+const { getEnergy, userExists, createUser } = require('../utils/energy-system');
+
+module.exports = {
+    name: 'energy',
+    admin: false,
+    description: '⚡ Check your energy',
+    
+    exec: async (sock, from, args, msg, isAdmin, sendWithTyping) => {
+        const userId = msg.key.remoteJid;
+        
+        if (!(await userExists(userId))) {
+            await createUser(userId);
+        }
+        
+        const energy = await getEnergy(userId);
+        
+        await sendWithTyping(sock, from,
+            `⚡ *Your Energy*\n\n💫 ${energy} energy`
+        );
+    }
+};
