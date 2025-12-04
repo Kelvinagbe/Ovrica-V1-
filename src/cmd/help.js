9eda22ac77557117ac0791dcdec4aef7015e4465
Kelvin Agbe <139677635+Kelvinagbe@users.noreply.github.com>
2025-12-04 20:12:28 +0100

    Update help.js

--- src/cmd/help.js
+++ src/cmd/help.js
@@ -1,23 +1,25 @@
-// commands/help.js
-
 const { templates } = require('../tmp/templates');
 
 module.exports = {
-
     name: 'help',
-
     admin: false,
-
     description: 'Show all commands',
 
     exec: async (sock, from, args, msg, isAdmin, sendWithTyping) => {
-
         const CONFIG = require('@/config');
-
         const text = templates.help(isAdmin);
 
-        await sendWithTyping(sock, from, { text });
-
+        await sendWithTyping(sock, from, { 
+            text,
+            contextInfo: {
+                forwardingScore: 999,
+                isForwarded: true,
+                forwardedNewsletterMessageInfo: {
+                    newsletterJid: "120363418958316196@newsletter",
+                    newsletterName: "🎭 Kelvin Tech",
+                    serverMessageId: 200
+                }
+            }
+        });
     }
-
 };
